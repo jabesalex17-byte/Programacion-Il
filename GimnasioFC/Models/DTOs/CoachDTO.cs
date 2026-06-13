73c7722156ddbb170ff2e13402044bda056b645c
Jabes Estevez <jabesalex17@gmail.com>
2026-06-13 15:20:11 -0400

    Adding DTOs for classes member and coach

---
+++ GimnasioFC/Models/DTOs/CoachDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace GimnasioFC.DTOs
+{
+    public class CoachDTO
+    {
+        [Required]
+        [JsonIgnore]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public DateTime HireDate { get; set; }
+        [Required]
+        public decimal Salary { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
