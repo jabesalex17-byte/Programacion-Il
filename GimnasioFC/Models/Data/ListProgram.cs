5d4671a0dbe52adf2ec7452c593c8ed1a6f84759
Jabes Estevez <jabesalex17@gmail.com>
2026-06-13 15:22:37 -0400

    adding core and listdata

---
+++ GimnasioFC/Models/Data/ListProgram.cs
@@ -0,0 +1,14 @@
+﻿using GimnasioFC.Controllers;
+using GimnasioFC.Models;
+
+namespace GimnasioFC.Data
+{
+    public class ListProgram
+    {
+        static public List<Member> Members = new List<Member>();
+        static public int Id = 1;
+
+        static public List<Coach> Coaches = new List<Coach>();
+        static public int cId = 1;
+    }
+}
