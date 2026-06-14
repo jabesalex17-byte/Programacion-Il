015753db63bac4942d29f3382171bc4dcec60a58
Jabes Estevez <jabesalex17@gmail.com>
2026-06-13 20:37:23 -0400

    Add files via upload

---
+++ GimnasioFC/Models/Data/DbContextGimnasioFC.cs
@@ -0,0 +1,11 @@
+﻿using GimnasioFC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GimnasioFC.Data
+{
+    public class DbContextGimnasioFC(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+    }
+}
