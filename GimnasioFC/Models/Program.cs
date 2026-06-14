9b5477332c85ddeec62425e6247659094ea50ce1
Jabes Estevez <jabesalex17@gmail.com>
2026-06-13 20:03:50 -0400

    Reformat Program.cs for consistency

--- GimnasioFC/Models/Program.cs
+++ GimnasioFC/Models/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.Logging.Abstractions;
-
+ 
 var builder = WebApplication.CreateBuilder(args);
  
 builder.Services.AddControllers();
