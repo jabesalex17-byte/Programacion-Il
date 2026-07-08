7957f6725d6945373df09341307236752b7ead64
Jabes Estevez <jabesalex17@gmail.com>
2026-07-08 18:03:10 -0400

    añadiendo core entities y reposotory a la capa

---
+++ GimnasioFC/GimnasioFC.Domain/Entities/Coach.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimnasioFC.Infrastructura.Models
+{
+    public class Coach
+    {
+        public string Specialty { get; set; } = string.Empty;
+
+        public decimal Salary { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
---
+++ GimnasioFC/GimnasioFC.Domain/Repository/ISubscriptionRepository.cs
@@ -0,0 +1,22 @@
+﻿using GimnasioFC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimnasioFC.Domain.Repository
+{
+    public interface ISubscriptionRepository
+    {
+        public Task<IEnumerable<Subscription>> GetAll();
+
+        public Subscription GetById(int id);
+
+        public Task Add(Subscription subscription);
+
+        public Task Update(Subscription subscription);
+
+        public Task Delete(int id);
+    }
+}
