4d490f4de15ab966d7a986a63952cf383c9e14d5
Jabes Estevez <jabesalex17@gmail.com>
2026-07-08 17:41:58 -0400

    añadiendo capa infraestrutura
    con sus interfaces y modelos

---
+++ GimnasioFC.Infrastructure/Repositories/MemberRepository.cs
@@ -0,0 +1,38 @@
+﻿using GimnasioFC.Domain.Repository;
+using GimnasioFC.Infrastructura.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimnasioFC.Infrastructura.Repositories
+{
+    internal class MemberRepository : IMemberRepository
+    {
+        public Task Add(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Member>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Member member)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GimnasioFC.Infrastructure/Repositories/SubcriptionRepository.cs
@@ -0,0 +1,38 @@
+﻿using GimnasioFC.Domain.Entities;
+using GimnasioFC.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimnasioFC.Infrastructura.Repositories
+{
+    internal class SubcriptionRepository : ISubscriptionRepository
+    {
+        public Task Add(Subscription subscription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Subscription>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Subscription GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Subscription subscription)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
