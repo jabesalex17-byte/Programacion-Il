937d92ff5e52adca3d1c35c4bff84a662f428a44
Jabes Estevez <jabesalex17@gmail.com>
2026-07-16 14:01:29 -0400

    Add files via upload

---
+++ GimnasioFC/GimnasioFC.Infrastructura/Context/GimnasioDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GimnasioFC.Domain.Entities;
+using GimnasioFC.Infrastructura.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimnasioFC.Infrastructura.Context
+{
+    public class GimnasioDbContext(DbContextOptions<GimnasioDbContext>  options) : DbContext(options)
+    {
+        public DbSet<MemberModel> Members { get; set; }
+        public DbSet<CoachModel> Coaches { get; set; }
+        public DbSet<SubscriptionModel> Subscriptions { get; set; }
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+
+}
---
+++ GimnasioFC/GimnasioFC.Infrastructura/Models/SubscriptionModel.cs
@@ -0,0 +1,22 @@
+﻿using GimnasioFC.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimnasioFC.Domain.Entities
+{
+    public class SubscriptionModel :BaseEntity
+    {
+        public int MemberId { get; set; }
+
+        public int MembershipId { get; set; }
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime FechaFin { get; set; }
+
+        public bool Activa { get; set; }
+    }
+}
