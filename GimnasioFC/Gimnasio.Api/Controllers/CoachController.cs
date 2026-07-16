d2e6bed4535ba7b1ef73a0e2f21a8f99f8a7133d
Jabes Estevez <jabesalex17@gmail.com>
2026-07-16 13:57:15 -0400

    Add files via upload

---
+++ GimnasioFC/Gimnasio.Api/Controllers/CoachController.cs
@@ -0,0 +1,46 @@
+﻿using GimnasioFC.Domain.Repository;
+using GimnasioFC.Infrastructura.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gimnasio.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoachController : ControllerBase
+    {
+        private readonly ICoachRepository _repo;
+
+        public CoachController(ICoachRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Coach>>> GetAllCoaches()
+        {
+            var coaches = await _repo.GetAllCoach();
+            return Ok(coaches);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddCoach(Coach coach)
+        {
+            await _repo.AddCoach(coach);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCoach(int id, Coach coach)
+        {
+            await _repo.UpdateCoach(id,coach);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteCoach(int id)
+        {
+            await _repo.DeleteCoach(id);
+            return Ok();
+        }
+    }
+}
