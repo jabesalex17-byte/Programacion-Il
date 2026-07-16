db2d8573f52a9395248bbff33c5142d3ab5a6d30
Jabes Estevez <jabesalex17@gmail.com>
2026-07-16 13:42:18 -0400

    actualizando la capa api

--- GimnasioFC/Gimnasio.Api/Program.cs
+++ GimnasioFC/Gimnasio.Api/Program.cs
@@ -1,22 +1,39 @@
+using GimnasioFC.Domain.Repository;
+using GimnasioFC.Infrastructura.Context;
+using GimnasioFC.Infrastructura.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+
+
+
+var MyConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<GimnasioDbContext>(option =>
+{
+    option.UseNpgsql(MyConnectionString);
+});
+
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+builder.Services.AddScoped<ICoachRepository, CoachRepository>();
+builder.Services.AddScoped<ISubscriptionRepository, SubcriptionRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//app.UseHttpsRedirection();
+app.MapSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+
 
 app.MapControllers();
 
