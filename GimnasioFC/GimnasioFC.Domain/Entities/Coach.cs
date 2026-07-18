39d45b41fdb6dc56a4c2c6186a7f071165a095cc
Jabes Estevez <jabesalex17@gmail.com>
2026-07-18 16:28:56 -0400

    Add files via upload

---
+++ GimnasioFC/GimnasioFC.Domain/Entities/Coach.cs
@@ -0,0 +1,18 @@
+﻿using GimnasioFC.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimnasioFC.Infrastructura.Models
+{
+    public class Coach : BasePerson 
+    {
+        public string Specialty { get; set; } = string.Empty;
+
+        public decimal Salary { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
