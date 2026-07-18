97f384ecd02b07b86d93be6905c99c37d22252e3
Jabes Estevez <jabesalex17@gmail.com>
2026-07-18 16:32:55 -0400

    Add files via upload

---
+++ GimnasioFC/GimnasioFC.Domain/Entities/Member.cs
@@ -0,0 +1,16 @@
+﻿using GimnasioFC.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GimnasioFC.Infrastructura.Models
+{
+    public class Member : BasePerson
+    {
+        public DateTime RegistrationDate { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
